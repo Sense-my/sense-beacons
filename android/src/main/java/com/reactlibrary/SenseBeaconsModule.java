7e576b54508d514369d132ef64bd35e369d0f17e
unknown <68980869+HalitKayarSense@users.noreply.github.com>
2021-10-21 14:50:24 +0800

    android

--- android/src/main/java/com/reactlibrary/SenseBeaconsModule.java
+++ android/src/main/java/com/reactlibrary/SenseBeaconsModule.java
@@ -2,10 +2,31 @@
 
 package com.reactlibrary;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.moko.beaconxpro.entity.BeaconXAxis;
+import com.moko.beaconxpro.entity.BeaconXInfo;
+import com.moko.beaconxpro.entity.BeaconXTH;
+import com.moko.beaconxpro.entity.BeaconXTLM;
+import com.moko.beaconxpro.entity.BeaconXUID;
+import com.moko.beaconxpro.entity.BeaconXURL;
+import com.moko.beaconxpro.entity.BeaconXiBeacon;
+import com.moko.beaconxpro.utils.BeaconXInfoParseableImpl;
+import com.moko.beaconxpro.utils.BeaconXParser;
+import com.moko.support.MokoBleScanner;
+import com.moko.support.MokoSupport;
+import com.moko.support.callback.MokoScanDeviceCallback;
+import com.moko.support.entity.DeviceInfo;
+
+import org.json.JSONArray;
+
+import java.security.Timestamp;
+import java.util.ArrayList;
+
 
 public class SenseBeaconsModule extends ReactContextBaseJavaModule {
 
@@ -21,9 +42,108 @@ public class SenseBeaconsModule extends ReactContextBaseJavaModule {
         return "SenseBeacons";
     }
 
+    ArrayList<DeviceInfo> beacons = new ArrayList<DeviceInfo>();
+    ArrayList<String> macs = new ArrayList<String>();
+    ArrayList<String> timestamps = new ArrayList<String>();
+
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void initilize(Callback callback) {
+        MokoSupport.getInstance().init(reactContext);
+        callback.invoke("Beacons Initilized");
+    }
+
+    @ReactMethod
+    public void getBeacons(Callback callback) {
+        String t = "";
+        for(int i = 0; i < beacons.size(); i++){
+            t += parseData(i,beacons.get(i));
+        }
+        callback.invoke(t == ""? beacons.size():t);
+    }
+
+    @ReactMethod
+    public void startScan(Callback callback) {
+        MokoBleScanner mokoBleScanner = new MokoBleScanner(reactContext);
+        mokoBleScanner.startScanDevice(new MokoScanDeviceCallback() {
+            @Override
+            public void onStartScan() {
+            }
+
+            @Override
+            public void onScanDevice(DeviceInfo device) {
+                Long tsLong = System.currentTimeMillis();
+                String ts = tsLong.toString();
+
+                if(!macs.contains(device.mac)){
+
+                    macs.add(device.mac);
+                    timestamps.add(ts);
+                    beacons.add(device);
+                }else{
+                    int index = macs.indexOf(device.mac);
+                    beacons.set(index, device);
+                    timestamps.set(index, ts);
+                }
+
+            }
+
+            @Override
+            public void onStopScan() {
+            }
+
+        });
+
+        callback.invoke("Beacons scan started");
+    }
+
+
+
+    String parseData(int index, DeviceInfo deviceInfo){
+        BeaconXInfoParseableImpl beaconXInfoParseable = new BeaconXInfoParseableImpl();
+        try{
+            BeaconXInfo beaconXInfo = beaconXInfoParseable.parseDeviceInfo(deviceInfo);
+            ArrayList<BeaconXInfo.ValidData> validDatas = new ArrayList<>(beaconXInfo.validDataHashMap.values());
+
+            String ytr = "";
+            for (BeaconXInfo.ValidData validData : validDatas) {
+                /*if (validData.type == BeaconXInfo.VALID_DATA_FRAME_TYPE_IBEACON) {
+                    BeaconXiBeacon beaconXiBeacon = BeaconXParser.getiBeacon(beaconXInfo.rssi, validData.data);
+                    major = beaconXiBeacon.major;
+                    minor = beaconXiBeacon.minor;
+                    uuid = beaconXiBeacon.uuid;
+                }*/
+
+                /*if (validData.type == BeaconXInfo.VALID_DATA_FRAME_TYPE_UID) {
+                    BeaconXUID beaconXUID = BeaconXParser.getUID(validData.data);
+                    //ytr = beaconXUID.instanceId;
+                }
+                if (validData.type == BeaconXInfo.VALID_DATA_FRAME_TYPE_URL) {
+                    BeaconXURL beaconXURL = BeaconXParser.getURL(validData.data);
+                    //ytr = beaconXURL.url;
+                }
+                if (validData.type == BeaconXInfo.VALID_DATA_FRAME_TYPE_TLM) {
+                    BeaconXTLM beaconXTLM = BeaconXParser.getTLM(validData.data);
+                    //ytr = beaconXTLM.adv_cnt;
+                }*/
+                if (validData.type == BeaconXInfo.VALID_DATA_FRAME_TYPE_IBEACON) {
+                    BeaconXiBeacon beaconXiBeacon = BeaconXParser.getiBeacon(beaconXInfo.rssi, validData.data);
+                    ytr = beaconXiBeacon.major+","+beaconXiBeacon.minor+","+beaconXiBeacon.uuid+","+beaconXInfo.rssi+","+timestamps.get(index)+"||";
+                }
+                /*if (validData.type == BeaconXInfo.VALID_DATA_FRAME_TYPE_TH) {
+                    BeaconXTH beaconXTH = BeaconXParser.getTH(validData.data);
+                    //ytr = beaconXTH.humidity;
+                }
+                if (validData.type == BeaconXInfo.VALID_DATA_FRAME_TYPE_AXIS) {
+                    BeaconXAxis beaconXAxis = BeaconXParser.getAxis(validData.data);
+                    //ytr = beaconXAxis.scale;
+                }*/
+            }
+            return ytr;
+            //return major+","+minor+","+uuid;
+        }catch(Exception exception){
+            return "";
+        }
+
+
     }
 }
--- index.js
+++ index.js
@@ -4,7 +4,67 @@ import { NativeModules } from 'react-native';
 
 const { SenseBeacons } = NativeModules;
 
-export function deb(){
-    console.log("dd",SenseBeacons);
+export function initilize() {
+    SenseBeacons.initilize(callback => {
+        console.log(callback);
+    });
+}
+var isScan = false;
+var beacons = {};
+export function scan() {
+    if (!isScan) {
+        isScan = true;
+        SenseBeacons.startScan(callback => {
+            
+            setInterval(function () {
+                setBeacons();
+            }, 1000);
+        });
+
+    }
+
+}
+
+function setBeacons() {
+    SenseBeacons.getBeacons(callback => {
+        
+        var tmp = "";
+        if(callback.length > 3){
+            tmp = callback.substring(0, callback.length - 2)
+            tmp = tmp.toString().split("||");
+        }else{
+            tmp = [];
+        }
+        
+        var newArray = {};
+        for (var i = 0; i < tmp.length; i++) {
+            var tmp2 = tmp[i].split(",");
+            try {
+                var curtime = new Date().getTime();
+                if (curtime - tmp2[4] <= 1500) {
+                    if (typeof beacons[tmp2[1]] != "undefined") {
+                        newArray[tmp2[1]] = beacons[tmp2[1]];
+                        newArray[tmp2[1]].rssi = tmp2[3];
+                    } else if (typeof tmp2[0] != "undefined") {
+                        newArray[tmp2[1]] = {};
+                        newArray[tmp2[1]].major = tmp2[0]
+                        newArray[tmp2[1]].minor = tmp2[1]
+                        newArray[tmp2[1]].uuid = tmp2[2]
+                        newArray[tmp2[1]].rssi = tmp2[3]
+                    }
+                }
+            } catch (e) {
+                console.log("issue", e)
+            }
+
+
+        }
+
+        beacons = newArray;
+    });
+}
+
+export function getBeacons() {
+    return beacons;
 }
 
